c774971c8b18db7d78c5b88dc9cb60e22e198edd
hyoungil.park <hyoungil.park@hnine.com>
2025-09-28 21:17:56 +0900

    라이브러리 살펴보기

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,7 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
+
+        Hello hello = new Hello();
+        hello.setData("hello");
+        String data = hello.getData();
+
+
+        SpringApplication.run(JpashopApplication.class, args);
+
 	}
 
 }
